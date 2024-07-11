e85da6fe4f7d0d775cfa7c9141abc56cd960a7ab
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-11 13:18:36 +0200

    Script that creates a table `users`.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,8 @@
+-- Script to create a table called `users`
+-- id: INT
+-- email: string
+-- name: string
+
 CREATE TABLE IF NOT EXISTS `users`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
