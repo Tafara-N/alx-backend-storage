dac974e7e6bf197bb4049ad07987c2d54f3178d3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-11 14:47:19 +0200

    Script that lists all bands with Glam rock as their main style, ranked by their longevity.

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+
+SELECT `band_name`, IFNULL(split, 2022) - formed AS `lifespan`
+FROM metal_bands
+WHERE FIND_IN_SET("Glam Rock", style)
+ORDER BY `lifespan` ASC;
