be124111a17b54ff7819fddfbf7eb2ec10189185
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-11 17:57:53 +0200

    Script creates a stored procedure `ComputeOverallScoreForUser` that computes and stores the overall score for a student.

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- Script creates a stored procedure ComputeOverallScoreForUser that computes
+-- and store the overall score for a student
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
+
+DELIMITER;
