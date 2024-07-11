a69fb17e3c022a5be7198ec30cb1c6ec53dd7f0e
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-11 18:28:02 +0200

    Script creates a STORED PROCEDURE `AddBonus` that adds a new correction FOR a student.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,24 +1,28 @@
 -- Script creates a STORED PROCEDURE `AddBonus` that adds a new correction FOR a student
 
-DELIMETER |
-CREATE PROCEDURE AddBonus (
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score FLOAT
-)
+DROP PROCEDURE IF EXISTS AddBonus;
 
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    INSERT INTO projects (name)
-    SELECT project_name FROM DUAL
-    WHERE NOT EXISTS (
-        SELECT * FROM projects WHERE name = project_name
-    );
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
 
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
+    END IF;
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
     INSERT INTO corrections(user_id, project_id, score)
-    VALUES(
-        user_id,
-        (SELECT id FROM projects WHERE name = project_name),
-        score
-        );
-END;
-|
+        VALUES (user_id, project_id, score);
+END $$
+
+DELIMITER;
