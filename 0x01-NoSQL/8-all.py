e18290f0192cfeacb5ac134350b044438d8ea893
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-12 18:32:06 +0200

    Function that lists all documents in a MongoDB collection.

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Function that lists all documents in a MongoDB collection.
+"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Parameters
+        mongo_collection: A list of MongoDB collections
+
+    Return
+        An empty list if no document in the collection
+    """
+
+    pass
