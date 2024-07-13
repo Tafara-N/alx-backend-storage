3aa9d0c10b2100083d6e6253ef2fe2672e9a6340
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-13 23:41:20 +0200

    Function that inserts a new document in a collection based on 'kwargs'.

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Function that inserts a new document in a collection based on `kwargs`
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Parameters
+        mongo_collection: pymongo collection object
+        kwargs: dictionary with data to insert
+
+    Return
+        _id of the new document
+    """
+
+    return mongo_collection.insert_one(kwargs).inserted_id
