37352eb174bddcf4b5a94cdc000ad9045ca996d8
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-17 16:14:23 +0200

    Function uses the requests module to obtain the HTML content of a particular URL and returns it.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,11 +1,71 @@
 #!/usr/bin/env python3
 
 """
-Function uses the requests module to obtain the HTML content of a particular URL and returns it
+Function uses the requests module to obtain the HTML content of a particular
+URL and returns it
 """
 
 import requests
+import redis
+from functools import wraps
 
 
+store = redis.Redis()
+
+def count_url_usage(method):
+    """
+    Decorator function to count URL usage and cache the data
+
+    Function caches the result of the method for a given URL
+
+    Parameters
+        url: URL for which the result should be cached
+
+    Return
+        str: Cached HTML content for the given URL
+    """
+
+    @wraps(method)
+    def wrapper(url):
+        """
+        Wrapper function to cache the result of the method for a given URL
+
+        Parameters
+            url: URL for which the result should be cached
+
+        Return
+            str: Cached HTML content for the given URL
+        """
+
+        cached_key = f"cached:{url}"
+        cached_data = store.get(cached_key)
+
+        if cached_data:
+            return cached_data.decode("utf-8")  # type: ignore
+
+        count_key = f"count:{url}"
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_usage
 def get_page(url: str) -> str:
-    pass
+    """
+    Simply counts the number of times the function was accessed
+
+    Retrieves the content of the given URL and returns it as a string
+
+    Parameters
+        url: The URL to retrieve the content from
+
+    Return
+        str: The content of the URL
+    """
+
+    response = requests.get(url)
+    return response.text
