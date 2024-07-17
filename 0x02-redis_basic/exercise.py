ad4ee4ef6ab74a5a635aac66e35ecd2a98b2452f
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-17 13:59:36 +0200

    Writing and Reading to/from Redis and recovering original type.

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Writing and Reading to/from Redis and recovering original type
+"""
+
+import redis
+import uuid
+
+from typing import Callable, Union
+
+
+class Cache():
+    """
+    Writing strings to Redis
+    """
+
+    def __init__(self) -> None:
+        """
+        Private redis instance and then flush the instance
+        """
+
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Parameters
+            self: Cache instance
+            data: Union[str, bytes, int, float] - data to store
+
+        Return
+            A string
+        """
+
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
