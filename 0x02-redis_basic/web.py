dc59d6e25dc4b4a3b2291c5a3c853e9bdc554333
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-17 12:40:19 +0200

    Function uses the requests module to obtain the HTML content of a particular URL and returns it.

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Function uses the requests module to obtain the HTML content of a particular URL and returns it
+"""
+
+import requests
+
+
+def get_page(url: str) -> str:
+    pass
